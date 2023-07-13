39a2a7cc383c81061e6cda0b624c4fdc45d9ae2d
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2023-07-13 17:47:53 +0700

    Update summarizator.py

--- source/summarizator.py
+++ source/summarizator.py
@@ -6,23 +6,29 @@ def req(s, num_return_sequences=6, num_beams=5, no_repeat_ngram_size=3, repetiti
         top_k=50, top_p=0.9, temperature=0.95, genstrategy="beamsearch"):
     try:
         response = requests.post("https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict", json={
-        "instances": [
-            {
-                "text": s,
-                "num_beams": num_beams,
-                "num_return_sequences": num_return_sequences,
-                "no_repeat_ngram_size": no_repeat_ngram_size,
-                "repetition_penalty": repetition_penalty,
-                "length_penalty": length_penalty,
-                "top_k": top_k,
-                "top_p": top_p,
-                "temperature": temperature,
-                "genstrategy": genstrategy
+            "instances": [
+                {
+                    "text": s,
+                    "num_beams": num_beams,
+                    "num_return_sequences": num_return_sequences,
+                    "no_repeat_ngram_size": no_repeat_ngram_size,
+                    "repetition_penalty": repetition_penalty,
+                    "length_penalty": length_penalty,
+                    "top_k": top_k,
+                    "top_p": top_p,
+                    "temperature": temperature,
+                    "genstrategy": genstrategy
 
-            }
-        ]
-    })
+                }
+            ]
+        })
+        
+        if response.json()['comment'] == "Ok!":
+            return response.json()['predictions']
+        else:
+            return response.json()['comment']
+
     except:
-        return 'Api error'
+        return "Server error"
 
-    return response.json()['prediction_best']['bertscore']
+
