927fb9c357e5a400d66be30594c5f3d7adcaa4a7
DelicaLib <danil-nikonov-2017@mail.ru>
2023-07-19 03:08:48 +0300

    summarizator frontend
    added functional without variants of results

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from source.summarizator import request as sumRequest
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/sendToSummarizatorModel", methods=["POST"])
+def sendToSummarizatorModel():
+    inputData = request.get_json()
+    return sumRequest(inputData)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- source/summarizator.py
+++ source/summarizator.py
@@ -1,17 +1,41 @@
 # -*- coding: utf-8 -*-
 import requests
 
+def parseInputData(inputData):
+    readyInputData = {}
+    if "text" in inputData:
+        readyInputData["text"] = inputData["text"]
+    if "genstrategy" in inputData:
+        readyInputData["genstrategy"] = inputData["genstrategy"]
+    if "num_beams" in inputData:
+        readyInputData["num_beams"] = int(inputData["num_beams"])
+    if "num_return_sequences" in inputData:
+        readyInputData["num_return_sequences"] = int(inputData["num_return_sequences"])
+    if "no_repeat_ngram_size" in inputData:
+        readyInputData["no_repeat_ngram_size"] = int(inputData["no_repeat_ngram_size"])
+    if "repetition_penalty" in inputData:
+        readyInputData["repetition_penalty"] = float(inputData["repetition_penalty"])  
+    if "length_penalty" in inputData:
+        readyInputData["length_penalty"] = float(inputData["length_penalty"])    
+    if "top_k" in inputData:
+        readyInputData["top_k"] = int(inputData["top_k"])
+    if "top_p" in inputData:
+        readyInputData["top_p"] = float(inputData["top_p"])
+    if "temperature" in inputData:
+        readyInputData["temperature"] = float(inputData["temperature"])
+    return readyInputData
 
-def req(s):
-        response = requests.post("https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict", json={
-            "instances": [
-               s
-            ]
-        })
-        
-        if response.json()['comment'] == "Ok!":
-            return response.json()
-        else:
-            return response.json()['comment']
+def request(inputData):
+    inputData = parseInputData(inputData)
+    response = requests.post("https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict", json={
+        "instances": [
+            inputData
+        ]
+    }).json()
+    if ("detail" in response):
+        return {"comment": "Ошибка"}
+    
+    if response['comment'] == "Ok!":
+        return response
 
-
+    return {"comment": response['comment']}
