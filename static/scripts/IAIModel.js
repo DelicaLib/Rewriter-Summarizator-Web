b3f46ff81851f5242ea940b739f84fd48512a067
DelicaLib <danil-nikonov-2017@mail.ru>
2023-07-24 15:31:19 +0300

    fix

--- static/scripts/IAIModel.js
+++ static/scripts/IAIModel.js
@@ -1,17 +1,16 @@
 class IAIModel {
-    static currentPage = 1
-    static currentResult = 1
-    static AIResults = []
-    static pageCount = 0
-    static changeExtraSettingMenuStatus() {}
-    static setModeSettings(object) {}
-    static setDisabledInputs(id, doDisabled) {}
-    static setSettingValue(settingId, available = true, value = 0) {}
-    static sendTextToModel() {}
-    static setResultVariant(variantNumber) {}
-    static setCurrentPageResult(pagenumber) {}
-    static setNextPageResult(pagenumber) {}
-    static setPageResult(pagenumber, buttons) {}
-    static nextPageResult() {}
-    static prevPageResult() {}
+    static currentPage = 1 // Номер текущей страницы с результатами
+    static currentResult = 1 // Номер текущего варианта
+    static AIResults = [] // Все варианты
+    static pageCount = 0 // Количество страниц
+    static changeExtraSettingMenuStatus() {} // Открыть/закрыть дополнительные настройки
+    static setModeSettings(object) {} // Выставить настройки для выбранного режима
+    static setDisabledInputs(id, doDisabled) {} // Отключить/включить input с настройкой
+    static setSettingValue(settingId, available = true, value = 0) {} // Выставить значение настройки
+    static sendTextToModel() {} // Отправить данные к API модели
+    static setResultVariant(variantNumber) {} // Установить вариант, как выбранный
+    static setCurrentPageResult(pagenumber) {} // Установить страницу, как текущую
+    static setPageResult(pagenumber, buttons) {} // Установить номера вариантов на кнопки
+    static nextPageResult() {} // Перейти на следующую страницу
+    static prevPageResult() {} // Перейти на предыдущую страницу
 }
